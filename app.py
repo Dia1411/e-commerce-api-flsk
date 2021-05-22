82c34131b9582b4addd85c11bd26c604de300b8b
Dia1411 <adishkembi14@gmail.com>
2021-05-22 13:37:46 +0200

    db

--- app.py
+++ app.py
@@ -629,7 +629,7 @@ def edit_products():
                                     ) 
                                 SELECT JSONB_AGG(photos)
                                 FROM new_photos 
-                                WHERE photos->>'photo_uuid' != '%s')) 
+                                WHERE photos->>'photo_uuid' != %s)) 
                             WHERE id = %s;
                             """
                     
