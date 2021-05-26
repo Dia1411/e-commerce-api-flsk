e7acf3c1a497398de16ee825770049b6f794b8aa
Dia1411 <adishkembi14@gmail.com>
2021-05-26 23:55:01 +0200

    email functionality addded

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ CORS(app)
 mail= Mail(app)
 
 app.config['MAIL_SERVER']='smtp.gmail.com'
-app.config['MAIL_PORT'] = 465
+app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = "stinesarah121@gmail.com"
 app.config['MAIL_PASSWORD'] = "11@235813"
 app.config['MAIL_USE_TLS'] = True
